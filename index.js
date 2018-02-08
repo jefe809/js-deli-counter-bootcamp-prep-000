a53019ff630c718a187e8d04b50e703a951db948
Jeffrey Damaso <jeffreydamaso340@gmail.com>
2018-02-08 22:16:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ function nowServing(deliLine){
 function currentLine(line){
   var lineNum = [];
   if (line.length === 0){
-    return `The line is currently empty`;
+    return `The line is currently empty.`;
   }
   for(let i = 0; i < line.length; i++){
-    lineNum.push(`${i + 1}.${line[i]}`);
+    lineNum.push(`${i + 1}. ${line[i]}`);
   }
   return "The line is currently: " + lineNum; 
 }   
